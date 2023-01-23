972a5918d8be040bd0dc0e37dd9eaaeacc62d2f3
Aziz1019 <starinc1998@gmail.com>
2023-01-23 15:04:52 +0500

    Initial commit

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,26 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        int days = 0;
+        switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 -> {
+                days = 31;
+                System.out.println(days);
+            }
+            case 4, 6, 9, 11 -> {
+                days = 30;
+                System.out.println(days);
+            }
+            case 2 -> {
+                if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+                    days = 29;
+                    System.out.println(days);
+                } else {
+                    days = 28;
+                    System.out.println(days);
+                }
+            }
+            default -> System.out.println("invalid date");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        if (dividend == 0 || divider == 0){
+            System.out.println("division by zero");
+        } else if (getRemainder(dividend, divider) == 0) {
+            System.out.println("can be divided completely");
+        } else if (getRemainder(dividend, divider) != 0) {
+            System.out.println("cannot be divided completely");
+        }
+    }
+
+    static int getRemainder(int num, int divisor)
+    {
+        return (num - divisor * (num / divisor));
+    }
+
+    public static void main(String[] args) {
+        IntegerDivider divider = new IntegerDivider();
+        divider.printCompletelyDivided(25, 0);
     }
 }
